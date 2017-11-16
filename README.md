9313de8bf96bf69d849e4af1494a29f5fab55803
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-16 14:17:13 -0600

    Clerical work

---
+++ README.md
@@ -0,0 +1,3 @@
+# math
+
+This provides a subset of the `rand` package that works efficiently on FPGAs.
--- rand/rand.go
+++ rand/rand.go
@@ -11,15 +11,3 @@ func RandUint32(seed uint32, output chan<- uint32) {
 		seed = c
 	}
 }
-
-func RandInt32(seed int32, output chan<- int32) {
-	for {
-		a := seed ^ (seed << 13)
-		b := a ^ (a >> 17)
-		c := b ^ (b << 5)
-		go func() {
-			output <- c
-		}()
-		seed = c
-	}
-}
