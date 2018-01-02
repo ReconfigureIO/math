fb5f4ac33049343906b8d8ea43886697210c97d9
Josh Bohde <josh@joshbohde.com>
2018-01-02 14:33:36 -0600

    Use a struct.

--- rand/rand.go
+++ rand/rand.go
@@ -1,6 +1,18 @@
 package rand
 
-func RandUint32(seed uint32, output chan<- uint32) {
+// A Rand is a source of random numbers
+type Rand struct {
+	seed uint32
+}
+
+// Construct a new Rand given a seed
+func New(seed uint32) Rand {
+	return Rand{seed: seed}
+}
+
+// Write a stream of uint32s to the given channel
+func (r Rand) Uint32s(output chan<- uint32) {
+	seed := r.seed
 	for {
 		a := seed ^ (seed << 13)
 		b := a ^ (a >> 17)
